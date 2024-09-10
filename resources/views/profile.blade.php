d23292b6212bb0194058230cfd9a80b8fb0f91ff
unknown <nadyamhrani@gmail.com>
2024-09-10 10:56:31 +0700

    Add route and view for profile with dynamic data

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profile Page</title>
+</head>
+<body>
+    <div class="profile-container">
+        <p>Nama : {{ $nama }}</p>
+        <p>Kelas : {{ $kelas }}</p>
+        <p>NPM : {{ $npm }}</p>
+    </div>
+</body>
+</html>

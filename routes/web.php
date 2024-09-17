6923837dbc2857a93b7b6ef4a76fb61de53e7a4a
unknown <nadyamhrani@gmail.com>
2024-09-17 20:35:29 +0700

    Add styling to create_user page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,7 +18,11 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/profile', [ProfileController::class, 'profile']);
+
+// Routes for ProfileController
+Route::get('/profile/{nama?}/{kelas?}/{npm?}', [ProfileController::class, 'profile']);
+
+// Routes for UserController
 Route::get('/user/profile', [UserController::class, 'profile']); 
 Route::get('/user/create', [UserController::class, 'create']);
 Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
